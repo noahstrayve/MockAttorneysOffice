ca98dc1911ebccdb5146161ab72877cdde81b00a
Noah Strayve <noahstrayve@gmail.com>
2018-05-24 18:12:21 -0500

    creating landing page

---
+++ src/components/landing/landing.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './GiveCards.css';
+
+
+const LandingPage = pages => {
+
+	return <div className="contentContainer">
+		<div className="landingWelcomeBox">
+			<div className="landingWelcomeImageBox">
+				
+			</div>
+			<div className="landingWelcomeOptionsBanner">
+				<div className="landingWelcomeOptionsBox">
+					<div className="landingWelcomeOptionsImage">
+					
+					</div>
+					<div className="landingWelcomeOptionsTextBox">
+						<div className="landingWelcomeOptionsHeadline">
+					
+						</div>
+						<div className="landingWelcomeOptionsText">
+					
+						</div>
+					</div>
+				</div>
+				<div className="landingWelcomeOptionsBox">
+					<div className="landingWelcomeOptionsImage">
+					
+					</div>
+					<div className="landingWelcomeOptionsTextBox">
+						<div className="landingWelcomeOptionsHeadline">
+					
+						</div>
+						<div className="landingWelcomeOptionsText">
+					
+						</div>
+					</div>
+				</div>
+				<div className="landingWelcomeOptionsBox">
+					<div className="landingWelcomeOptionsImage">
+					
+					</div>
+					<div className="landingWelcomeOptionsTextBox">
+						<div className="landingWelcomeOptionsHeadline">
+					
+						</div>
+						<div className="landingWelcomeOptionsText">
+					
+						</div>
+					</div>
+				</div>
+				<div className="landingWelcomeOptionsBox">
+					<div className="landingWelcomeOptionsImage">
+					
+					</div>
+					<div className="landingWelcomeOptionsTextBox">
+						<div className="landingWelcomeOptionsHeadline">
+					
+						</div>
+						<div className="landingWelcomeOptionsText">
+					
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>;
+}
+
+export default LandingPage;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navigation = props => {
+	return <div className="navbarContainer">
+		<div className="navbarTitleBox">
+			<h2 className="navbarTitle">DuGud</h2>
+		</div>
+		<div className="navbarLinkContainer">
+			<div className="navbarLinkBox">
+				<p className="navbarLinkText">AAAAAAAAA</p>
+			</div>
+			<div className="navbarLinkBox">
+				<p className="navbarLinkText">BBBBBBBBB</p>
+			</div>
+			<div className="navbarLinkBox">
+				<p className="navbarLinkText">CCCCCCCCC</p>
+			</div>
+		</div>
+	</div>;
+}
+
+export default Navigation;
