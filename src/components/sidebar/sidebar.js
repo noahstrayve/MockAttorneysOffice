51249f6e54954f2935d9af3e5a37ef0b556cde06
Noah Strayve <noahstrayve@gmail.com>
2018-06-04 18:14:34 -0500

    final push of the day

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -4,21 +4,21 @@ import { Link } from "react-router-dom";
 
 const Sidebar = props => {
 	return <div className="sidebarBox">
-			<div className="sidebarOptionBox">
-				<Link to="/about"><div className="sidebarOptionText">
+			<Link to="/about"><div className="sidebarOptionBox">
+				<div className="sidebarOptionText">
 					<p>Firm History</p>
-				</div></Link>
-			</div>
-			<div className="sidebarOptionBox">
-				<Link to="/contact"><div className="sidebarOptionText">
+				</div>
+			</div></Link>
+			<Link to="/contact"><div className="sidebarOptionBox">
+				<div className="sidebarOptionText">
 					<p>Contact Us</p>
-				</div></Link>
-			</div>
-			<div className="sidebarOptionBox">
-				<Link to="/team"><div className="sidebarOptionText">
+				</div>
+			</div></Link>
+			<Link to="/team"><div className="sidebarOptionBox">
+				<div className="sidebarOptionText">
 					<p>Our Team</p>
-				</div></Link>
-			</div>
+				</div>
+			</div></Link>
 		</div>;
 }
 
