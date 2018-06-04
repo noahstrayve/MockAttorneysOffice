c95a6f8fcde9c20fe830fe12ca1bdbbce3d1d6a9
Noah Strayve <noahstrayve@gmail.com>
2018-06-04 17:35:05 -0500

    changing repo name pushing changes

--- src/components/team/team.js
+++ src/components/team/team.js
@@ -6,7 +6,18 @@ import { Link } from "react-router-dom";
 const Attorneys = pages => {
 
 	return <div className="employeeInfoWrapper">
-			<h1>Attorneys</h1>
+				<div className="employeeInfoTitleBox">
+					<div className="employeeInfoTitle">
+						<h1>Attorneys</h1>
+					</div>
+				</div>
+				<div className="employeeInfoTextBox">
+					<div className="employeeInfoText">
+						
+					</div>
+
+				</div>
+			
 			</div>;
 }
 
